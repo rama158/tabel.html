8198f87bb5b228bb81790a91915412d06f0a93ef
rama158 <ramadhaniagungsaputra41@gmail.com>
2025-01-15 10:27:23 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,17 @@
+
+let nama = "rama"; 
+let alamat = "sayung"; 
+let usia = "15";  
+let ttl = "Demak 5 september 2009"; 
+let gender = "laki laki"; 
+let agama = "islam"; 
+let email = "ramadhaniagungsaputra41@gmail.com";
+let berat = "59";
+let tinggi = "165";
+let hobi = "main game";
+let cita = "orang sukses";
+let pekerjaan = "siswa";
+let sekolah = "SMK N 1 SAYUNG";
+let jurusan = "PPlg";
+
+document.write("nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + "tahun, saya lahir di " + ttl + "jenis kelamin saya adalah " + gender + "saya beragama " + agama + "alamat email saya adalah " + email + "saya memiliki berat badan " + berat +"kg"+ "dengan tinggi " + tinggi + "saya sangat senang " + hobi + "karena saya memiliki cita cita " + cita + "saya saat adalah seorang " + pekerjaan + "yang bersekolah di " + sekolah + "pada jurusan " + jurusan);
